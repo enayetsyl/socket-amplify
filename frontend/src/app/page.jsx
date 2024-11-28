fbbee24aedb3caef5bd7162c2a2d6dca8a360a97
enayetaitech <mdrehman@365aitech.com>
2024-11-28 15:22:52 +0600

    first commit

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <div className="flex justify-center items-center min-h-screen">
+    <h1 className="text-center text-5xl text-red-600">This is next.js app</h1>
+   </div>
+
+   </>
+  );
+}
